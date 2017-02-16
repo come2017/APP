9feb5034fe71332e78f1bd19aed40fdd81fbaa5d
278162542 <278162542@qq.com>
2017-02-16 17:04:16 +0800

    优化

--- js/area.js
+++ js/area.js
@@ -14,15 +14,41 @@
 			
 			//初始化页面链接
 			this.href();
-			
-			
+								
 		},
 		
 		//DOM节点
 		doc : {
 			funBox : document.querySelector(".fun-box"),
 			
-			head : document.querySelector("header")
+			head : document.querySelector("header"),
+			
+			funMenu : document.querySelector(".fun-menu"),
+			
+			funSearch : document.querySelector(".fun-search"),
+			
+			funSort : document.querySelectorAll(".fun-sort"),
+			
+			swiper : document.querySelector(".swiper"),
+			
+			funSub : document.querySelector(".fun-sub")
+			
+		},
+		
+		//侧滑栏显示
+		swiperShow : function(){
+			
+			var offCanvasWrapper = mui('.swiper');			
+			offCanvasWrapper.offCanvas('show');
+			
+		},
+		
+		//侧滑栏隐藏
+		swiperHide : function(){
+			
+			var offCanvasWrapper = mui('.swiper');			
+			offCanvasWrapper.offCanvas('close');
+			
 		},
 		
 		//点击事件
@@ -30,16 +56,41 @@
 			var _this = this;
 			var a= document.querySelectorAll(".fun-Clist a");
 			
-			mui('.fun-Clist').on('tap','a',function(index){
-				
+			//全部分类
+			mui('.fun-Clist').on('tap','a',function(index){				
 				for(var i=0; i<a.length; i++ ){
 					a[i].className = "";	
 				}			
-				this.className = 'active';
-				
+				this.className = 'active';				
 			});
 			
+			//菜单按钮绑定侧滑栏显示
+			_this.doc.funMenu.addEventListener("tap" , _this.swiperShow );
+			
+			//fun-search绑定策划栏显示			
+			_this.doc.funSearch.addEventListener("tap" , _this.swiperShow );
+			
+			//提交 重置 绑定hide隐藏
+			_this.doc.funSub.addEventListener("tap" , _this.swiperHide );
+			
+			//排序按钮绑定事件
+			var li = document.querySelectorAll(".fun-sort li");
+			
+			for(var i=0; i<li.length; i++){
+				li[i].addEventListener("tap" , function(){
+					
+					var off = arguments.callee.off = !arguments.callee.off 
+					
+					if( off ){
+						this.querySelector("i").className = "triangle-up";
+					}else{						
+						this.querySelector("i").className = "triangle-down";
+					}
+					
+				})
+			}			 						
 		},
+		
 		
 		//页面链接
 		href : function(){
@@ -53,8 +104,7 @@
 			}) 
 			
 		},
-		
-		
+				
 		//MUI 初始化
 		mui_init : function(){
 			mui('.mui-scroll-wrapper').scroll({
